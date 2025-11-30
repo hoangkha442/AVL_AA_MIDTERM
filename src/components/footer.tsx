3f562a99ad9ff4359bf9f97f8d22fbffe1726e61
Hoang Kha <chauhoangkha442@gmail.com>
2025-11-30 20:46:42 +0700

    upload footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,16 @@
-type Props = {}
+type Props = {};
 
 export default function Footer({}: Props) {
   return (
-          <div className="fixed z-50 bottom-1.5 left-0 right-0">
-            <div className="flex justify-center">
-                <div className="mt-3 text-xs font-medium text-white border border-white/10 bg-white/5 shadow-[0_30px_90px_rgba(0,0,0,0.75)] backdrop-blur px-4 p-1 rounded-4xl">
-                Team 01: Quí - My - Kha 
-              </div>
-              </div>
-          </div>
-  )
+    <div className="fixed z-50 bottom-1.5 left-0 right-0">
+      <div className="flex justify-center gap-2">
+        <div className="mt-3 text-xs font-medium text-white border border-white/10 bg-white/5 shadow-[0_30px_90px_rgba(0,0,0,0.75)] backdrop-blur px-4 p-1 rounded-4xl">
+          Team 01: Quí - My - Kha
+        </div>
+        <div className="mt-3 text-xs font-medium text-white border border-white/10 bg-white/5 shadow-[0_30px_90px_rgba(0,0,0,0.75)] backdrop-blur px-4 p-1 rounded-4xl">
+          © khadeptrai.dev
+        </div>
+      </div>
+    </div>
+  );
 }
